cad2ad32be029d6a78d82e65e0b5af4d6873e000
AppleDash <pet@feather.horse>
2020-08-12 04:49:03 -0400

    Initial commit, add magic_bulk

---
+++ README.md
@@ -0,0 +1,7 @@
+twi-tools
+=========
+A collection of random programs I've written to assist me with managing a particular image booru.
+
+## magic\_bulk
+Runs `magic_file()` on a bunch of files and figures out if they're valid images or not.
+
---
+++ magic_bulk.c
@@ -0,0 +1,103 @@
+/* magic_bulk: takes in a list of image files on stdin, runs magic on them all.
+ * writes successfully identified files to one file, unsuccessfuls to another. */
+#include <magic.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define FLUSH 1 // whether or not to flush the files after each line written
+#define MAX_FILE_LENGTH 1024 // seems reasonable, linux uses 4096 but that's like, a lot, dude.
+
+magic_t magic; // magic_t is a pointer type
+
+FILE *goodFp; // fp where "good" filenames are written
+FILE *badFp; // fp where names of "bad" files are written
+
+bool good_magic_info(const char *magicInfo) {
+    if (!magicInfo) {
+        return false;
+    }
+
+    // accept any image or video MIME type.
+    return strstr(magicInfo, "image/") == magicInfo ||
+            strstr(magicInfo, "video/") == magicInfo;
+}
+
+void process(const char *filename) {
+    const char *magicInfo = magic_file(magic, filename);
+    FILE *fp = good_magic_info(magicInfo) ? goodFp : badFp;
+
+    if (!fprintf(fp, "%s\n", filename)) {
+        fprintf(stderr, "failed to write to file!");
+        return;
+    }
+
+    
+    #if FLUSH
+        fflush(fp);
+    #endif
+}
+
+int main(int argc, char *argv[]) {
+    int retval = 0;
+    char buf[MAX_FILE_LENGTH + 1] = { 0 };
+
+    if (argc != 3) {
+        fprintf(stderr, "usage: %s <good file> <bad file>\n", argv[0]);
+        return 1;
+    }
+
+    magic = magic_open(MAGIC_SYMLINK | MAGIC_MIME_TYPE); // follow symlinks because why not, we want MIME types.
+
+    if (!magic || (magic_load(magic, NULL)) != 0) {
+        fprintf(stderr, "failed to create magic handle\n");
+        return 1;
+    }
+
+    goodFp = fopen(argv[1], "a");
+
+    if (!goodFp) {
+        fprintf(stderr, "failed to open \"good\" file %s\n", argv[1]);
+
+        magic_close(magic);
+        return 1;
+    }
+
+    badFp = fopen(argv[2], "a");
+
+    if (!badFp) {
+        fprintf(stderr, "failed to open \"bad\" file %s\n", argv[2]);
+
+        fclose(goodFp);
+        magic_close(magic);
+        return 1;
+    }
+
+    while (fgets(buf, sizeof(buf), stdin) != 0) { // null on error or EOF
+        int length = strlen(buf);
+        bool good = false;
+
+        while (length > 0 && (buf[length - 1] == '\r' || buf[length - 1] == '\n')) { // get rid of any newlines
+            buf[length - 1] = 0;
+            length--;
+        }
+
+        if (length > 0) {
+            process(buf);
+        }
+
+    }
+
+    if (ferror(stdin)) {
+        fprintf(stderr, "error reading from stdin\n");
+
+        retval = 1;
+    }
+
+
+    magic_close(magic);
+    fclose(goodFp);
+    fclose(badFp);
+
+    return retval;
+}
